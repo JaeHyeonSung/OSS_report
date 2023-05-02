4a26fc7e42f446ebf3122ac4fcaf1e7efd3f9712
JaeHyeonSung <top91652@gmail.com>
2023-05-02 17:46:49 +0900

    added root function

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -3,7 +3,7 @@
 # Department of Civil Engineering
 # Delhi Technological University (formerly, Delhi College of Engineering)
 # New Delhi, India
-
+import math
 
 #Python program to create a simple calculator 
   
@@ -22,34 +22,47 @@ def multiply(number1, number2):
 #Function to perform division of two numbers 
 def divide(number1, number2): 
     return (number1 / number2)
-  
-print("Operation Menu -\n" \ 
-        "1. Addition of two numbers\n" \ 
-        "2. Subtraction of two numbers\n" \ 
-        "3. Multiplication of two numbers\n" \ 
+#Function to perform root of any numbers
+def root(number1):
+    return (math.sqrt(number1))
+print("Operation Menu -\n" 
+        "1. Addition of two numbers\n" 
+        "2. Subtraction of two numbers\n" 
+        "3. Multiplication of two numbers\n" 
         "4. Division of two numbers\n") 
   
   
 #Taking input from the user  
 select = input("Select operations form 1, 2, 3, 4 :") 
   
-num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: ")) 
   
 if select == '1': 
-    print(num1, "+", numb2, "=", 
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print(num1, "+", num2, "=",
                     add(num1, num2)) 
   
 elif select == '2': 
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
     print(num1, "-", num2, "=", 
                     subtract(num1, num2)) 
   
-elif select == '3': 
+elif select == '3':
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
     print(num1, "*", num2, "=", 
                     multiply(num1, num2)) 
   
-elif select == '4': 
-    print(num1, "/", num2, "=", 
-                    divide(num1, num2)) 
+elif select == '4':
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(num1, "/", num2, "=",)
+                    divide(num1, num2))
+elif select == '5':
+    num1 = int(input("Enter the number: "))
+    print("root", num1, "=", root(num1))
 else: 
-    print("Invalid input!"
+    print("Invalid input!")
